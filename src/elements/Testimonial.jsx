b5b32bdaf7a728fe9b88f4bac63c7f74e210e558
Emmanuel <noelzappy@gmail.com>
2023-08-27 01:44:54 +0000

    chore: fixed testimonies

--- src/elements/Testimonial.jsx
+++ src/elements/Testimonial.jsx
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
+const Testimonies = [
+  {
+    description:
+      "With their expertise, they crafted a seamless platform that empowers us to sell lumber online. The user-friendly interface and efficient features have streamlined our operations and expanded our reach.",
+    name: "Richard",
+    position: "CEO, Geofrelin Company LTD",
+    image: "/assets/images/client/richard.webp",
+  },
+];
+
 class Testimonial extends Component {
   render() {
     return (
@@ -7,34 +18,35 @@ class Testimonial extends Component {
         <div className="row">
           <div className="col-lg-12">
             <Tabs>
-              <TabPanel>
-                <div className="rn-testimonial-content text-center">
-                  <div className="inner">
-                    <p>
-                      Aklima The standard chunk of Lorem Ipsum used since the
-                      1500s is reproduced below for those interested. Sections
-                      Bonorum et Malorum original.
-                    </p>
-                  </div>
-                  <div className="author-info">
-                    <h6>
-                      <span>Aklima </span> - COO, AMERIMAR ENTERPRISES, INC.
-                    </h6>
-                  </div>
-                </div>
-              </TabPanel>
+              {Testimonies.map((item, i) => {
+                return (
+                  <TabPanel key={i}>
+                    <div className="rn-testimonial-content text-center">
+                      <div className="inner">
+                        <p>{item.description}</p>
+                      </div>
+                      <div className="author-info">
+                        <h6>
+                          <span>{item.name} </span> -{item.position}
+                        </h6>
+                      </div>
+                    </div>
+                  </TabPanel>
+                );
+              })}
 
               <TabList className="testimonial-thumb-wrapper">
-                <Tab>
-                  <div className="testimonial-thumbnai">
-                    <div className="thumb">
-                      <img
-                        src="/assets/images/client/testimonial-1.jpg"
-                        alt="Testimonial Images"
-                      />
-                    </div>
-                  </div>
-                </Tab>
+                {Testimonies.map((item, i) => {
+                  return (
+                    <Tab key={i}>
+                      <div className="testimonial-thumbnai">
+                        <div className="thumb">
+                          <img src={item.image} alt={item.name} />
+                        </div>
+                      </div>
+                    </Tab>
+                  );
+                })}
               </TabList>
             </Tabs>
           </div>
