697f37feb1ee4100adfdec96a83bdc47ff47d524
Emmanuel Yeboah <noelzappy@gmail.com>
2023-10-01 15:28:32 +0000

    chore: updated website with brand colors and old logo

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -11,7 +11,26 @@ class Header extends Component {
     window.addEventListener("load", function () {
       console.log("All assets are loaded");
     });
+    this.state = {
+      scrolled: false,
+    };
   }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (window.scrollY > 80) {
+      this.setState({ scrolled: true });
+    } else {
+      this.setState({ scrolled: false });
+    }
+  };
 
   menuTrigger() {
     document.querySelector(".header-wrapper").classList.toggle("menu-open");
@@ -34,61 +53,73 @@ class Header extends Component {
       }
     }
     const { color = "default-color" } = this.props;
+    const { scrolled } = this.state;
+
+    console.log(scrolled);
 
     const logoUrl = (
-      <img src="/assets/images/logo/logo.png" alt="Digital Agency" />
+      <img
+        src="/assets/images/logo/logo.png"
+        alt="Wordnox Logo"
+        // style={scrolled ? { width: "170px" } : {}}
+        // className={scrolled ? "sticky-logo" : ""}
+      />
     );
 
     return (
-      <header
-        className={`header-area formobile-menu header--transparent ${color}`}
-      >
-        <div className="header-wrapper" id="header-wrapper">
-          <div className="header-left">
-            <div className="logo">
-              <a href="/">{logoUrl}</a>
+      <div className={`${scrolled ? "sticky sticky-bg" : ""}`}>
+        <header
+          className={`header-area formobile-menu ${
+            scrolled ? "header--static" : "header--transparent"
+          } ${color}`}
+        >
+          <div className="header-wrapper" id="header-wrapper">
+            <div className="header-left">
+              <div className="logo">
+                <a href="/">{logoUrl}</a>
+              </div>
+            </div>
+            <div className="header-right ">
+              <nav className="mainmenunav d-lg-block">
+                <ul className="mainmenu">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/portfolio">Our Works</Link>
+                  </li>
+                  <li>
+                    <Link to="/service">Services</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About Us</Link>
+                  </li>
+                </ul>
+              </nav>
+              <div className="header-btn">
+                <a className="rn-btn" href="/contact">
+                  <span>Get in touch</span>
+                </a>
+              </div>
+              {/* Start Humberger Menu  */}
+              <div className="humberger-menu d-block d-lg-none pl--20">
+                <span
+                  onClick={this.menuTrigger}
+                  className="menutrigger text-white"
+                >
+                  <FiMenu />
+                </span>
+              </div>
+              {/* End Humberger Menu  */}
+              <div className="close-menu d-block d-lg-none">
+                <span onClick={this.CLoseMenuTrigger} className="closeTrigger">
+                  <FiX />
+                </span>
+              </div>
             </div>
           </div>
-          <div className="header-right">
-            <nav className="mainmenunav d-lg-block">
-              <ul className="mainmenu">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/portfolio">Our Works</Link>
-                </li>
-                <li>
-                  <Link to="/service">Services</Link>
-                </li>
-                <li>
-                  <Link to="/about">About Us</Link>
-                </li>
-              </ul>
-            </nav>
-            <div className="header-btn">
-              <a className="rn-btn" href="/contact">
-                <span>Get in touch</span>
-              </a>
-            </div>
-            {/* Start Humberger Menu  */}
-            <div className="humberger-menu d-block d-lg-none pl--20">
-              <span
-                onClick={this.menuTrigger}
-                className="menutrigger text-white"
-              >
-                <FiMenu />
-              </span>
-            </div>
-            {/* End Humberger Menu  */}
-            <div className="close-menu d-block d-lg-none">
-              <span onClick={this.CLoseMenuTrigger} className="closeTrigger">
-                <FiX />
-              </span>
-            </div>
-          </div>
-        </div>
-      </header>
+        </header>
+      </div>
     );
   }
 }
