8da04365e0454f387a6f12e0d973a15f4b90ac98
Emmanuel Yeboah <noelzappy@gmail.com>
2024-05-04 00:17:45 +0000

    chore: updated colors to fit edromart

--- components/common/Logo.js
+++ components/common/Logo.js
@@ -1,6 +1,31 @@
+import Image from "next/image";
 import { Constants } from "../../helpers/constants";
 
+// <Image
+//   src="/images/logo.png"
+//   alt="Wordnox Software Solutions"
+//   width={40}
+//   height={40}
+// />
 const Logo = () => {
+  return (
+    <div className="logo d-flex align-items-center justify-items-center">
+      <h6 className="logo-text display-6 text-bold">
+        <strong>
+          Word
+          <mark
+            style={{
+              backgroundColor: Constants.color.yellow,
+              color: "#000248",
+            }}
+          >
+            nox
+          </mark>
+        </strong>
+      </h6>
+    </div>
+  );
+
   return (
     <svg width="281px" height="60px" viewBox="0 0 281 60" version="1.1">
       <title>Wordnox Software Solutions</title>
