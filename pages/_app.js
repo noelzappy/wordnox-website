58c9e0ab704d04d5b291e410b4d9cb858b9537e3
Emmanuel Yeboah <noelzappy@gmail.com>
2024-06-07 03:23:13 +0000

    chore: added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,9 @@ import "../assets/css/plugins/plugins.css";
 import "../assets/scss/style.scss";
 import "react-loading-skeleton/dist/skeleton.css";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
+import * as gtag from "../helpers/gtag";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -21,6 +24,26 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <NextTopLoader />
       <Toaster />
       <Component {...pageProps} />
